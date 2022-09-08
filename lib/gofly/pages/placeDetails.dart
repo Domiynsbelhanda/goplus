4631883575bc06b22a5a13550d4f4c2a5edbdf83
Dominique Youness <youness.dominique@gmail.com>
2022-09-08 13:15:39 +0200

    [STyle] details screens

--- lib/gofly/pages/placeDetails.dart
+++ lib/gofly/pages/placeDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:goplus/gofly/widgets/app_widgets/app_button.dart';
 
 class PlaceDetailScreen extends StatefulWidget{
 
@@ -13,11 +14,102 @@ class PlaceDetailScreen extends StatefulWidget{
 }
 
 class _PlaceDetailScreen extends State<PlaceDetailScreen>{
+
+  late Size size;
+
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
     // TODO: implement build
     return Scaffold(
-      body: Text('${this.widget.place['title']}'),
+      body: Container(
+        height: size.height,
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: size.height / 1.18,
+                padding: EdgeInsets.all(10.0),
+                width: size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          '${this.widget.place['imagePath']}'
+                      )
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: size.height / 1.18,
+                padding: EdgeInsets.all(10.0),
+                width: size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                    gradient: LinearGradient(
+                        begin: FractionalOffset.topCenter,
+                        end: FractionalOffset.bottomCenter,
+                        colors: [
+                          Colors.grey.withOpacity(0.0),
+                          Colors.black.withOpacity(.5),
+                        ],
+                        stops: [
+                          0.0,
+                          1.0
+                        ]
+                    )
+                ),
+              ),
+            ),
+
+            Positioned(
+              bottom: size.height - ((size.height / 1.18)),
+                left: 32,
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${this.widget.place['title']}',
+                        style: TextStyle(
+                            fontSize: size.width / 15,
+                            color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+
+                      SizedBox(height: 4.0,),
+
+                      Text(
+                        '${this.widget.place['description']}',
+                        style: TextStyle(
+                          fontSize: size.width / 20,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+            ),
+
+            Positioned(
+              bottom: 16.0,
+              child: Container(
+                width: size.width,
+                child: AppButton(
+                  name: 'RESERVER',
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
