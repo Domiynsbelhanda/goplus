8c4bb24ac3a0c03f935e91398d0a494893159ace
Dominique Youness <youness.dominique@gmail.com>
2022-09-29 10:13:08 +0200

    [FEAT] Reading firebase data

--- lib/taxi/pages/driverTrackingPage.dart
+++ lib/taxi/pages/driverTrackingPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+const double ZOOM = 1;
 
 class DriverTrackingPage extends StatefulWidget{
   @override
@@ -9,11 +13,61 @@ class DriverTrackingPage extends StatefulWidget{
 }
 
 class _DriverTrackingPage extends State<DriverTrackingPage>{
+
+  static GoogleMapController? _googleMapController;
+  Set<Marker> markers = Set();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
+    return SafeArea(
+      child: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("location").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            //Extract the location from document
+            double latitude = snapshot.data!.docs.first.get('latitude');
+            double longitude = snapshot.data!.docs.first.get('longitude');
+            GeoPoint location = GeoPoint(latitude, longitude);
 
+            // Check if location is valid
+            if (location == null) {
+              return Text("There was no location data");
+            }
+
+            // Remove any existing markers
+            markers.clear();
+
+            final latLng = LatLng(location.latitude, location.longitude);
+
+            // Add new marker with markerId.
+            markers
+                .add(Marker(markerId: MarkerId("location"), position: latLng));
+
+            // If google map is already created then update camera position with animation
+            _googleMapController?.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: latLng,
+                zoom: ZOOM,
+              ),
+            ));
+
+            return GoogleMap(
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(location.latitude, location.longitude)),
+              // Markers to be pointed
+              markers: markers,
+              onMapCreated: (controller) {
+                // Assign the controller value to use it later
+                _googleMapController = controller;
+              },
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
