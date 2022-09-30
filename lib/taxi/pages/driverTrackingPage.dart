a8b42b91da334c69ce9c959c9363aa988e10fc78
Dominique Youness <youness.dominique@gmail.com>
2022-09-30 16:26:15 +0200

    [FEAT] Check driver is online

--- lib/taxi/pages/driverTrackingPage.dart
+++ lib/taxi/pages/driverTrackingPage.dart
@@ -118,76 +118,75 @@ class _DriverTrackingPage extends State<DriverTrackingPage>{
           stream: FirebaseFirestore.instance.collection("drivers").snapshots(),
           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasData) {
-              //Extract the location from document
               var data = snapshot.data!.docs;
               for(var i = 0; i < data.length; i++){
-                double latitude = data[i].get('latitude');
-                double longitude = data[i].get('longitude');
-                GeoPoint location = GeoPoint(latitude, longitude);
+                if(data[i].get('online')){
+                  double latitude = data[i].get('latitude');
+                  double longitude = data[i].get('longitude');
+                  GeoPoint location = GeoPoint(latitude, longitude);
 
-                // Check if location is valid
-                if (location == null) {
-                  return Text("There was no location data");
-                }
-                final latLng = LatLng(location.latitude, location.longitude);
+                  // Check if location is valid
+                  if (location == null) {
+                    return Text("There was no location data");
+                  }
+                  final latLng = LatLng(location.latitude, location.longitude);
 
-                // Add new marker with markerId.
-                markers
-                    .add(
-                    Marker(
-                      markerId: MarkerId("location"),
-                      position: latLng,
-                      icon: markerbitmap,
-                      onTap: (){
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return Container(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.all(16.0),
-                                    height: MediaQuery.of(context).size.width / 2.5,
-                                    child: Image.network(
-                                      '${data[i].get('profpic')}'
-                                    ),
+                  // Add new marker with markerId.
+                  markers
+                      .add(
+                      Marker(
+                          markerId: MarkerId("location"),
+                          position: latLng,
+                          icon: markerbitmap,
+                          onTap: (){
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Container(
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        margin: const EdgeInsets.all(16.0),
+                                        height: MediaQuery.of(context).size.width / 2.5,
+                                        child: Image.network(
+                                            '${data[i].get('profpic')}'
+                                        ),
+                                      ),
+                                      Text(
+                                        '${data[i].get('firstn')} ${data[i].get('lastn')} ${data[i].get('midn')}',
+                                        style: TextStyle(
+                                            fontSize: 20.0
+                                        ),
+                                      ),
+                                      Text(
+                                          'A ${calculateDistance(widget.depart, latLng).toStringAsFixed(2)} mètre(s)'
+                                      ),
+
+                                      SizedBox(height: 16.0,),
+
+                                      AppButton(
+                                        name: 'RESERVER',
+                                        onTap: (){
+                                          Navigator.pop(context);
+                                          FirebaseFirestore.instance.collection('drivers').doc(data[i].id).update({
+                                            'ride': true,
+                                            'depart_longitude': widget.depart.longitude,
+                                            'depart_latitude': widget.depart.latitude,
+                                            'destination_longitude': widget.destination.longitude,
+                                            'destination_latitude': widget.destination.latitude,
+                                            'distance': calculateDistance(widget.depart, latLng).toStringAsFixed(2)
+                                          });
+                                        },
+                                      )
+                                    ],
                                   ),
-                                  Text(
-                                      '${data[i].get('firstn')} ${data[i].get('lastn')} ${data[i].get('midn')}',
-                                    style: TextStyle(
-                                      fontSize: 20.0
-                                    ),
-                                  ),
-                                  Text(
-                                    'A ${calculateDistance(widget.depart, latLng).toStringAsFixed(2)} mètre(s)'
-                                  ),
-
-                                  SizedBox(height: 16.0,),
-
-                                  AppButton(
-                                    name: 'RESERVER',
-                                    onTap: (){
-                                      Navigator.pop(context);
-                                      FirebaseFirestore.instance.collection('drivers').doc(data[i].id).update({
-                                        'ride': true,
-                                        'depart_longitude': widget.depart.longitude,
-                                        'depart_latitude': widget.depart.latitude,
-                                        'destination_longitude': widget.destination.longitude,
-                                        'destination_latitude': widget.destination.latitude,
-                                        'distance': calculateDistance(widget.depart, latLng).toStringAsFixed(2)
-                                      });
-                                    },
-                                  )
-                                ],
-                              ),
+                                );
+                              },
                             );
-                          },
-                        );
-                      }
-                    )
-                );
-
-                position = latLng;
+                          }
+                      )
+                  );
+                }
               }
 
               return Stack(
