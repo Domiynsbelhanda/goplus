ad166ccbcd7dd738f90aefe6edaefcb2384efef6
Dominique Youness <youness.dominique@gmail.com>
2022-10-03 11:47:24 +0200

    [FEAT] Add phone call

--- lib/taxi/pages/google_maps_popylines.dart
+++ lib/taxi/pages/google_maps_popylines.dart
@@ -90,27 +90,27 @@ class _Poly extends State<GoogleMapsPolylines> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Stack(
+        child : StreamBuilder(
+            stream: FirebaseFirestore.instance.collection("drivers").doc(widget.id).snapshots(),
+            builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              var data = snapshot.data!.data() as Map<String, dynamic>;
+              _markers.clear();
+              _markers.add(
+                  Marker(
+                    markerId: MarkerId('DriverPosition'),
+                    position: LatLng(data['latitude'], data['longitude']),
+                    infoWindow: const InfoWindow(
+                      title: 'Driver Position',
+                      snippet: '',
+                    ),
+                    icon: markerbitmap!,
+                  )
+              );
+
+              return Stack(
           children: [
             SafeArea(
-              child : StreamBuilder(
-              stream: FirebaseFirestore.instance.collection("drivers").doc(widget.id).snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-                var data = snapshot.data!.data() as Map<String, dynamic>;
-                _markers.clear();
-                _markers.add(
-                    Marker(
-                      markerId: MarkerId('DriverPosition'),
-                      position: LatLng(data['latitude'], data['longitude']),
-                      infoWindow: const InfoWindow(
-                        title: 'Driver Position',
-                        snippet: '',
-                      ),
-                      icon: markerbitmap!,
-                    )
-                );
-
-                return GoogleMap(
+              child : GoogleMap(
                   initialCameraPosition: _kGoogle,
                   mapType: MapType.normal,
                   markers: _markers,
@@ -132,8 +132,7 @@ class _Poly extends State<GoogleMapsPolylines> {
                     );
                     _controller.complete(ctrl);
                   },
-                );
-              })
+                )
             ),
 
             Positioned(
@@ -141,8 +140,36 @@ class _Poly extends State<GoogleMapsPolylines> {
               top: 16,
               child: CloseButtons(context),
             ),
+
+            Positioned(
+                bottom: 32,
+                left: 16,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 1.5,
+                  decoration: const BoxDecoration(
+                      color: Colors.white
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        const Icon(
+                            Icons.call
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        Text(
+                            '+243${data['phone']}'
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+            ),
           ],
-        ),
+        );
+            })
       ),
     );
   }
