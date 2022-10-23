394ae137b8838810d9fcd129ffca7e1ca86a3a35
Dominique Youness <youness.dominique@gmail.com>
2022-10-23 15:36:47 +0200

    [STYLE] Mise à jour information description appli

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -124,7 +124,7 @@ class _HomePage extends State<HomePage>{
                     Padding(
                       padding: const EdgeInsets.only(left: 16.0, top: 16.0, bottom: 16.0),
                       child: Text(
-                          'Nos taxis vous y conduirons en toute sécurité et convivialité à bord.',
+                          'Nos taxis vous y conduiront en toute sécurité et convivialité à bord.',
                           style: TextStyle(
                               fontSize: size.width / 25
                           )
