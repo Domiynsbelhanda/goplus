9a28326b711ea27cb8a47b97a321500962745675
Youness Dominique <youness.dominique@gmail.com>
2022-11-10 16:00:23 +0200

    [FEAT] shadow to find driver

--- lib/pages/driverTrackingPage.dart
+++ lib/pages/driverTrackingPage.dart
@@ -599,7 +599,15 @@ class _DriverTrackingPage extends State<DriverTrackingPage>{
         width: MediaQuery.of(context).size.width * 0.8,
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(24)
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
